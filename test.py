5064f0d55f64afb337c3ea928e94fdeb64d124b6
Lukas Nemec <lukas.nemec2@firma.seznam.cz>
2015-08-26 10:16:49 +0200

    Completed sqlite test

--- test.py
+++ test.py
@@ -11,9 +11,33 @@ import os
 DB_FILE = os.path.expanduser('~/.convertdb')
 
 
+def create_table(cursor):
+    """
+    Creates the table with index on input_path column.
+    """
+    cursor.execute('''
+        CREATE TABLE convert
+        (input_path text, output_path text, converted bool)
+    ''')
+    cursor.execute('CREATE INDEX input_path_index ON convert (input_path);')
+
+
+def table_exists(cursor):
+    """
+    Checks wheter the table already exists or should be created.
+    """
+    cursor.execute('SELECT name FROM sqlite_master WHERE type=\'table\'')
+    table = cursor.fetchone()
+
+    return table and 'convert' in table
+
+
 def main():
     conn = sqlite3.connect(DB_FILE)
+    c = conn.cursor()
 
+    if not table_exists(c):
+        create_table(c)
 
 
 if __name__ == '__main__':
