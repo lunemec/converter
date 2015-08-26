f20c44b0ecaccebd03ac116bd070a14990e3c695
Lukas Nemec <lukas.nemec2@firma.seznam.cz>
2015-08-26 14:11:59 +0200

    Added inserts and updates and file checking

--- test.py
+++ test.py
@@ -17,9 +17,9 @@ def create_table(cursor):
     """
     cursor.execute('''
         CREATE TABLE convert
-        (input_path text, output_path text, converted bool)
+        (input text, converted bool)
     ''')
-    cursor.execute('CREATE INDEX input_path_index ON convert (input_path);')
+    cursor.execute('CREATE INDEX input_index ON convert (input);')
 
 
 def table_exists(cursor):
@@ -32,12 +32,57 @@ def table_exists(cursor):
     return table and 'convert' in table
 
 
+def should_convert(cursor, input):
+    """
+    Checks if we should start converting:
+        - if the file exists: previous convert was interrupted, try again
+        - we haven't converted yet, convert
+    Or we should not:
+        - file has already been converted
+
+    @return bool
+    """
+    cursor.execute('SELECT converted FROM convert WHERE input=?', (input,))
+    result = cursor.fetchone()
+
+    if result is None:
+        return True
+
+    return not bool(result and result[0])
+
+
+def started_converting(cursor, input):
+    """
+    Writes a record into database that we started converting the file.
+    """
+    cursor.execute('INSERT INTO convert VALUES (?, 0)', (input,))
+
+
+def finished_converting(cursor, input):
+    """
+    Writes a record signaling we have completed converting the file.
+    """
+    cursor.execute('UPDATE convert SET converted=1 WHERE input=?', (input,))
+
+
 def main():
     conn = sqlite3.connect(DB_FILE)
     c = conn.cursor()
 
     if not table_exists(c):
         create_table(c)
+
+    for input in ['testfile']:
+        if should_convert(c, input):
+            started_converting(c, input)
+            conn.commit()
+            print('Would convert {}'.format(input))
+            finished_converting(c, input)
+            conn.commit()
+        else:
+            print('Already converted {}'.format(input))
+
+    conn.close()
 
 
 if __name__ == '__main__':
